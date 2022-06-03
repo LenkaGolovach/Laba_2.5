1170b6774ee6b208423883e64cb7d17d07f12659
LenkaGolovach <karamba290@gmail.com>
2022-06-03 11:27:11 +0300

    Выполненное задание

--- Ind.py
+++ Ind.py
@@ -4,8 +4,9 @@
 import sys
 
 if __name__ == '__main__':
-    A = list(map(int, input().split()))
-    if not A:
-        print("Заданный список пуст", file=sys.stderr)
-        exit(1)
-
+    A = tuple(map(int, input("Введите стоимость - ").split()))
+    B = tuple(map(int, input("Введите мощность - ").split()))
+    for id, val in enumerate(B):
+        if val <= 80:
+            print("Автомобиль, мощность которого не превышает 80 л.с. Его цена - ",
+                  A[id], " его мощность в л.с. - ", B[id])
